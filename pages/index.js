96826152223bc396fbcfcf050282dacfecad088f
Muhamad Ramdhany <muhamad_ramdhany@mitsuilease.co.id>
2025-09-05 19:08:40 +0700

    revisi deploy code

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// pages/index.js
+
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Selamat Datang di GJNet</h1>
+      <p>Halaman ini berhasil ditampilkan di Vercel.</p>
+    </div>
+  );
+}
